3d568a388a32e4704393487a43021c399db98b68
Dominika Malinowska <domin_mal@wp.pl>
2018-03-26 13:18:06 +0200

    hnvghnvghc

--- pierchem-app/src/test/java/com/s14014/tau/domain/PierwiastekTest.java
+++ pierchem-app/src/test/java/com/s14014/tau/domain/PierwiastekTest.java
@@ -4,13 +4,13 @@ package com.s14014.tau.domain;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-import com.s14014.tau.repository.IPierwiastekRepository;
 
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import com.s14014.tau.repository.*;
 
 public class PierwiastekTest {
 
@@ -36,24 +36,23 @@ public class PierwiastekTest {
 
     @Test
     public void updateTest(){
-        Pierwiastek pierwiastekToUpdate = pierwiastekRepository.getPierwiastekById(1);
+        Pierwiastek pierwiastekToUpdate = pierwiastekRepository.getPierwiastekById(3);
 
-        pierwiastekToUpdate.setNazwa("Wodór");
+        pierwiastekToUpdate.setNazwa("siarka");
         pierwiastekRepository.updateById(pierwiastekToUpdate);
 
-        assertEquals(pierwiastekRepository.getPierwiastekById(1).getNazwa(), pierwiastekToUpdate.getNazwa());
-        assertThat(pierwiastekToUpdate.getNazwa(), is("Wodór"));
+        assertEquals(pierwiastekRepository.getPierwiastekById(3).getNazwa(), pierwiastekToUpdate.getNazwa());
+        assertNotNull(pierwiastekRepository.getPierwiastekById(1));
 
-        Pierwiastek pierwiastek = pierwiastekRepository.getPierwiastekById(3);
-        assertThat(pierwiastek.getNazwa(), not("Wodór"));
+
     }
 
     @Test
     public void findTest(){
-        Pierwiastek pierwiastek = pierwiastekRepository.getPierwiastekById(1);
+       Pierwiastek pierwiastek = pierwiastekRepository.getPierwiastekById(2);
 
         assertNotNull(pierwiastek);
-        assertEquals("wodór", pierwiastek.getNazwa());
+        assertEquals("lit", pierwiastekRepository.getPierwiastekById(2).getNazwa());
     }
 
     @Test
@@ -69,7 +68,7 @@ public class PierwiastekTest {
         }
 
         catch(IndexOutOfBoundsException aIndexOutOfBoundsException){
-            assertThat(aIndexOutOfBoundsException.getMessage(), is("Index: 0, Size: 0"));
+            assertThat(aIndexOutOfBoundsException.getMessage(), is("Index: 3, Size: 3"));
         }
     }
 
@@ -82,19 +81,25 @@ public class PierwiastekTest {
         List<Pierwiastek> pierwiastki = pierwiastekRepository.getAllPierwiastki();
 
         assertNull(pierwiastekRepository.getPierwiastekById(1).getNazwa());
-        assertNotEquals(true, pierwiastki.isEmpty());
+        assertEquals(true, !pierwiastki.isEmpty());
     }
 
     
     @Before
     public void initRepository(){
-        Pierwiastek wodor = new Pierwiastek(1, "wodór", 1, 1, 1);
-        Pierwiastek hel = new Pierwiastek(2, "hel", 1, 18, 2);
-        Pierwiastek lit = new Pierwiastek(3, "lit", 2, 1, 1);
+       pierwiastekRepository = PierwiastekRepositoryFactory.getInstance();
+        Pierwiastek wodor = new Pierwiastek(0, "wodor", 1, 1, 1);
+
+        Pierwiastek hel = new Pierwiastek(1, "hel", 1, 18, 2);
+        Pierwiastek lit = new Pierwiastek(2, "lit", 2, 1, 1);
+        Pierwiastek magnez = new Pierwiastek(3, "magnez", 2, 2, 2);
+        Pierwiastek siarka = new Pierwiastek(4, "siarka", 2, 13, 3);
 
         pierwiastekRepository.add(wodor);
         pierwiastekRepository.add(hel);
         pierwiastekRepository.add(lit);
+        pierwiastekRepository.add(magnez);
+        pierwiastekRepository.add(siarka);
     }
 
 
